2e25b0f9453e130d92339cfd5066e643d0d9f954
bilsa <developer@bilsadeveloper.(none)>
2013-02-08 16:24:10 +0200

    Rails project initialized.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+School::Application.config.secret_token = '08d01c0fc13f30516195d69d4143cac8bf910e03f79fac84aadfb138b2e25f8f7d710a53a75be476222e5c4a766f2b6ef7142f6504d826db668c37018a2db1da'
