efc67499da5bd749cda7dbcafa81df90b8681ac9
cenbek <sa.ordekci@gmail.com>
2013-02-08 17:07:32 +0200

    students_list steps added.

---
+++ features/steps/students_list.rb
@@ -0,0 +1,13 @@
+class Spinach::Features::StudentsList < Spinach::FeatureSteps
+  step 'there are some students' do
+    pending 'create some records for students'
+  end
+
+  step 'I go to the students list page' do
+    pending 'visit the students list page'
+  end
+
+  step 'I should see students list' do
+    pending 'check the list is there'
+  end
+end
